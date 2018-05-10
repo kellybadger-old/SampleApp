c3684a6060ec46c81b14d67a8c61eeeebc28a675
Kelly Badger <kbadger@placemedia.com>
2018-05-10 10:00:30 -0600

    Added feature 1.

--- SampleApp/SampleApp/Feature1.cs
+++ SampleApp/SampleApp/Feature1.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace SampleApp
 {
-	class Program
+	class Feature1
 	{
-		static void Main(string[] args)
-		{
-			// Created sample application
-			// Added feature 1 class
-		}
 	}
 }
